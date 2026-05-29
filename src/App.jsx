3044913e529e7fe2e08af615d4677bf163d037d5
chua05 <chuakyla21@gmail.com>
2026-05-29 22:46:55 +0800

    new

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-green-600 text-white text-4xl p-10">
+      MENRO Reforestation Monitoring System
+    </div>
+  )
+}
+
+export default App
