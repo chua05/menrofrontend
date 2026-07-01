8b6b05457eb213f5b3bf410c162b6f3371fd1da5
geumaxh <maxeneduavepereira325@gmail.com>
2026-07-02 00:04:49 +0800

    refactor: update App.jsx imports to match renamed page files

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,18 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import DashboardPage from "./pages/DashboardPage";
 
-// Admin
-import ManageUsers from "./pages/admin/ManageUsers";
-
-// Staff
-import ManageSeedlings from "./pages/staff/ManageSeedlings";
-import ProcessRequests from "./pages/staff/ProcessRequests";
-import VerifyReports from "./pages/staff/VerifyReports";
-
-// Shared
-import PlantingEvents from "./pages/shared/PlantingEvents";
-import EventSchedule from "./pages/shared/EventSchedule";
-import SubmitPlantingReport from "./pages/shared/SubmitPlantingReport";
-import TreeMonitoring from "./pages/shared/TreeMonitoring";
-import PlantingMap from "./pages/shared/PlantingMap";
-import Reports from "./pages/shared/Reports";
-import SeedlingRequests from "./pages/shared/SeedlingRequests";
+// Pages
+import UsersPage from "./pages/UsersPage";
+import SeedlingsPage from "./pages/SeedlingsPage";
+import SeedlingRequestsPage from "./pages/SeedlingRequestsPage";
+import ProcessRequestsPage from "./pages/ProcessRequestsPage";
+import VerifyReportsPage from "./pages/VerifyReportsPage";
+import EventsPage from "./pages/EventsPage";
+import EventSchedulePage from "./pages/EventSchedulePage";
+import SitesPage from "./pages/SitesPage";
+import PlantingPage from "./pages/PlantingPage";
+import MonitoringPage from "./pages/MonitoringPage";
+import ReportsPage from "./pages/ReportsPage";
 
 function App() {
   return (
@@ -42,17 +38,17 @@ function App() {
             </ProtectedRoute>
           }>
             <Route path="dashboard" element={<DashboardPage />} />
-            <Route path="users" element={<ManageUsers />} />
-            <Route path="requests" element={<SeedlingRequests />} />
-            <Route path="seedlings" element={<ManageSeedlings />} />
-            <Route path="process-requests" element={<ProcessRequests />} />
-            <Route path="events" element={<PlantingEvents />} />
-            <Route path="schedule" element={<EventSchedule />} />
-            <Route path="map" element={<PlantingMap />} />
-            <Route path="planting" element={<SubmitPlantingReport />} />
-            <Route path="verify-reports" element={<VerifyReports />} />
-            <Route path="monitoring" element={<TreeMonitoring />} />
-            <Route path="reports" element={<Reports />} />
+            <Route path="users" element={<UsersPage />} />
+            <Route path="seedlings" element={<SeedlingsPage />} />
+            <Route path="requests" element={<SeedlingRequestsPage />} />
+            <Route path="process-requests" element={<ProcessRequestsPage />} />
+            <Route path="events" element={<EventsPage />} />
+            <Route path="schedule" element={<EventSchedulePage />} />
+            <Route path="sites" element={<SitesPage />} />
+            <Route path="planting" element={<PlantingPage />} />
+            <Route path="verify-reports" element={<VerifyReportsPage />} />
+            <Route path="monitoring" element={<MonitoringPage />} />
+            <Route path="reports" element={<ReportsPage />} />
           </Route>
 
           {/* Staff routes */}
@@ -62,16 +58,16 @@ function App() {
             </ProtectedRoute>
           }>
             <Route path="dashboard" element={<DashboardPage />} />
-            <Route path="seedlings" element={<ManageSeedlings />} />
-            <Route path="requests" element={<SeedlingRequests />} />
-            <Route path="process-requests" element={<ProcessRequests />} />
-            <Route path="events" element={<PlantingEvents />} />
-            <Route path="schedule" element={<EventSchedule />} />
-            <Route path="map" element={<PlantingMap />} />
-            <Route path="planting" element={<SubmitPlantingReport />} />
-            <Route path="verify-reports" element={<VerifyReports />} />
-            <Route path="monitoring" element={<TreeMonitoring />} />
-            <Route path="reports" element={<Reports />} />
+            <Route path="seedlings" element={<SeedlingsPage />} />
+            <Route path="requests" element={<SeedlingRequestsPage />} />
+            <Route path="process-requests" element={<ProcessRequestsPage />} />
+            <Route path="events" element={<EventsPage />} />
+            <Route path="schedule" element={<EventSchedulePage />} />
+            <Route path="sites" element={<SitesPage />} />
+            <Route path="planting" element={<PlantingPage />} />
+            <Route path="verify-reports" element={<VerifyReportsPage />} />
+            <Route path="monitoring" element={<MonitoringPage />} />
+            <Route path="reports" element={<ReportsPage />} />
           </Route>
 
           {/* Volunteer routes */}
@@ -81,12 +77,12 @@ function App() {
             </ProtectedRoute>
           }>
             <Route path="dashboard" element={<DashboardPage />} />
-            <Route path="request-seedlings" element={<SeedlingRequests />} />
-            <Route path="my-requests" element={<SeedlingRequests />} />
-            <Route path="events" element={<PlantingEvents />} />
-            <Route path="my-sites" element={<PlantingMap />} />
-            <Route path="my-activities" element={<SubmitPlantingReport />} />
-            <Route path="monitoring" element={<TreeMonitoring />} />
+            <Route path="request-seedlings" element={<SeedlingRequestsPage />} />
+            <Route path="my-requests" element={<SeedlingRequestsPage />} />
+            <Route path="events" element={<EventsPage />} />
+            <Route path="my-sites" element={<SitesPage />} />
+            <Route path="my-activities" element={<PlantingPage />} />
+            <Route path="monitoring" element={<MonitoringPage />} />
           </Route>
 
         </Routes>
